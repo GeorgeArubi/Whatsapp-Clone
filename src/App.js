ce02426e1b5049ff72a7dece0d5e8bcc4bc934db
DOGHOUSETV <gea53899@marymount.edu>
2020-12-28 18:13:16 -0500

    Build components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a MERN Whatsapp</h1>
+      <Sidebar />
+      <Chat />
     </div>  
   );
 }
