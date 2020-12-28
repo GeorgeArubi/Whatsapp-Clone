36ea31c140cf0f8b74580a4159d632d9baf03405
DOGHOUSETV <gea53899@marymount.edu>
2020-12-28 18:02:22 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyAParj87zTTFSaAgeu0XhF07YLVlBFJvI4",
+    authDomain: "whatsapp-mern-bbdd4.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-bbdd4.firebase.io",
+    projectId: "whatsapp-mern-bbdd4",
+    storageBucket: "whatsapp-mern-bbdd4.appspot.com",
+    messagingSenderId: "440814600187",
+    appId: "1:440814600187:web:0076f0de3289970377155e"
+  };
