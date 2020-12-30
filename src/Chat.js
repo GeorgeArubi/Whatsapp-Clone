5c21e73789efc98b9217d64ce4b4ca357bba3c28
DOGHOUSETV <gea53899@marymount.edu>
2020-12-30 13:11:34 -0500

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>Chat Component</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p>
+                    <span className="chat__name">Gbenga</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
