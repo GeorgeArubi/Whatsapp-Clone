8f8725547ac53765004f16ae0c71a0c2fefa569c
DOGHOUSETV <gea53899@marymount.edu>
2021-01-02 00:50:45 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.use(express.json())
 
 // db config
 const connection_url = 'mongodb+srv://admin:h5xHGvUxAcBqsLwx@cluster0.0s8su.mongodb.net/whatsapp_db?retryWrites=true&w=majority'
+// TODO replace mongo URI with defaukt placeholders
 
 mongoose.connect(connection_url, {
     useCreateIndex: true,
