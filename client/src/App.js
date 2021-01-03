5dc3da09fef48f0f0003566a6e58cf1f8623e2d4
DOGHOUSETV <gea53899@marymount.edu>
2021-01-02 20:14:09 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('32a73c3067223bcb5c05', {
+      cluster: 'us2'
+    })
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    })
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
