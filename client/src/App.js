4f5704dc0ddf13813ff12a7f83b4fd575db357b9
DOGHOUSETV <gea53899@marymount.edu>
2021-01-02 20:52:06 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
-import Pusher from'pusher-js';
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get('/messages/sync').then(response => {
+        setMessages(response.data)
+    })
+  }, [])
 
   useEffect(() => {
     const pusher = new Pusher('32a73c3067223bcb5c05', {
@@ -12,10 +20,18 @@ function App() {
     })
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage])
     })
-  }, [])
+
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages])
+
+  console.log(messages)
 
   return (
     <div className="app">
